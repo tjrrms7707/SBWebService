c6e14940fde7c720c492f80012aac7f05a6dff71
GEUN <tjrrms7707@icloud.com>
2022-03-15 22:50:44 +0900

    게시판 CRUD 완료 및 view 완료

--- SBWebService/src/main/java/org/sb/service/springboot/web/IndexController.java
+++ SBWebService/src/main/java/org/sb/service/springboot/web/IndexController.java
@@ -1,13 +1,35 @@
 package org.sb.service.springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.sb.service.springboot.service.PostsService;
+import org.sb.service.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id,Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts",dto);
+        return "posts-update";
     }
 }
